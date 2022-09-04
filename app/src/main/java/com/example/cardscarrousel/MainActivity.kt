1f64a22eab0bbc5ddd2ed9fbe8336accf80bb50f
Daniel Valencia <dfvalenciav@gmail.com>
2022-09-04 15:16:24 +0200

    Downgrade gradle version to 7.1.0 for fixing realm transform issue

--- app/src/main/java/com/example/cardscarrousel/MainActivity.kt
+++ app/src/main/java/com/example/cardscarrousel/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.cardscarrousel
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.navigation.findNavController
+import com.example.cardscarrousel.base.BaseActivity
+import com.example.cardscarrousel.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+@AndroidEntryPoint
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+    override fun inflateView(inflater: LayoutInflater) =
+        ActivityMainBinding.inflate(inflater)
 
+    override fun setUI() {
+        binding.navHostFragment.findNavController()
+            .setGraph(R.navigation.navigation)
     }
 }
--- app/src/main/java/com/example/cardscarrousel/cardCarrousel/presentation/cardCarrousel/CardCarrouselFragment.kt
+++ app/src/main/java/com/example/cardscarrousel/cardCarrousel/presentation/cardCarrousel/CardCarrouselFragment.kt
@@ -8,9 +8,11 @@ import androidx.fragment.app.viewModels
 import com.example.cardscarrousel.base.BaseFragment
 import com.example.cardscarrousel.cardCarrousel.presentation.models.CardCarrouselPresentation
 import com.example.cardscarrousel.databinding.FragmentCardCarrouselBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class CardCarrouselFragment : BaseFragment<FragmentCardCarrouselBinding, CardCarrouselViewModel>(){
-
     override val viewModel by viewModels<CardCarrouselViewModel> ()
     private lateinit var itemsCarrousel : List<CardCarrouselPresentation>
 
